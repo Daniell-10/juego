67e6d4073a13727a85a90b8f0661511e0cb30a56
Daniell-10 <iva.dan.lon.pin@gmail.com>
2025-03-27 01:30:07 +0000

    juego_actualizado

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1lR8biULrNX_TNVkdRBf1N7ycXV8eVGw",
+  authDomain: "juego-af928.firebaseapp.com",
+  projectId: "juego-af928",
+  storageBucket: "juego-af928.firebasestorage.app",
+  messagingSenderId: "544525848841",
+  appId: "1:544525848841:web:f15ecaeedfeac7874503d8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
